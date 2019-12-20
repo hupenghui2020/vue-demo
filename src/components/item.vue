4c5595d8fd0a47e045af7b949d8a18453d4d4be1
hph <1049970895@qq.com>
2019-12-20 18:23:15 +0800

    添加模块的交互
    添加模块的交互

---
+++ src/components/item.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="list-item editingClass editing">
+    <!-- 最外层容器-->
+    <label class="checkbox">
+      <!--自定义的多选框-->
+      <input type="checkbox" v-model="item.checked" />
+      <!--item.checked-->
+      <span class="checkbox-custom"></span>
+    </label>
+    <input type="text" v-model="item.text" placeholder="写点什么。。。" />
+    <!--绑定item.text-->
+    <a class="delete-item">
+      <!--删除图标-->
+      <span class="icon-trash"></span>
+    </a>
+  </div>
+</template>
+<script>
+// item 是todo的子组件,他接受一个对象item,来进行处理
+export default {
+  props: {
+    item: {
+      type: Object,
+      default: () => {
+        return {
+          checked: false,
+          text: '你好,世界'
+        }
+      }
+    }
+  }
+}
+</script>
+<style lang="less">
+@import "../common/style/list-items.less";
+</style>
--- src/components/layouts.vue
+++ src/components/layouts.vue
@@ -2,19 +2,22 @@
   <section class="container">
     <!--最外层容器-->
     <section class="menu">
-        <menus></menus>
       <!--左边的容器-->
+        <menus></menus>
     </section>
     <section class="content-container">
       <!--右边的容器-->
+      <todo></todo>
     </section>
   </section>
 </template>
 <script>
 import menus from './menus' // * 导入刚才我们创建的 menus组件
+import todo from './todo'
 export default {
   components: { // * 注册menus组件，让其可以在template调用
-    menus
+    menus,
+    todo // 右边栏的上面显示的东西
   }
 }
 </script>
