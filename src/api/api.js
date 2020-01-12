9c1f65c0546410d95a09fd4758f9daf5edef146f
hupenghui <1049970895@qq.com>
2020-01-12 23:28:09 +0800

    修改路由地址
    修改路由地址

--- src/api/api.js
+++ src/api/api.js
@@ -1,15 +1,15 @@
 import axios from 'axios' // 导入axios模块
-export const getTodoList = params => { // 封装一个函数，名为getTodoList
-  return axios.get(`/todo/list`, { // 请求路径 ‘/todo/list’
+export const getMenuList = params => { // 封装一个函数，名为getTodoList
+  return axios.get(`/menu/list`, { // 请求路径 ‘/todo/list’
     params: params
   })
 }
-export const addTodo = params => {
-  return axios.post(`/todo/add`, params).then(res => res.data)
+export const addMenu = params => {
+  return axios.post(`/menu/add`, params).then(res => res.data)
 }
 // 新增一个请求查询代办单项列表的数据的接口函数
 export const getTodo = params => {
-  return axios.get(`/todo/` + params.id)
+  return axios.get(`/menu/` + params.id)
 }
 // 新增一个请求新增代办单的接口函数
 export const addRecord = params => {
--- src/components/menus.vue
+++ src/components/menus.vue
@@ -12,28 +12,29 @@
       <span class="count-list" v-if="item.count > 0">{{item.count}}</span>
       {{item.title}}
     </a>
-    <a class="link-list-new" @click="addTodoList">
+    <a class="link-list-new" @click="addMenuList">
       <span class="icon-plus"></span>
       新增
     </a>
   </div>
 </template>
 <script>
-import { getTodoList, addTodo } from '../api/api' // 引入我们 封装好的两个接口函数。
+import { getMenuList, addMenu } from '../api/api' // 引入我们 封装好的两个接口函数。
 export default {
   data () {
     return {
       items: [], // 菜单数据
-      todoId: '' // 默认选中id
+      todoId: '', // 默认选中id
+      isShow: true
     }
   },
   created () {
     // 调用请求菜单列表数据的接口
-    getTodoList({}).then(res => {
-      const menuBars = res.data // 数据都会返回在res.data里面。
-      if (menuBars !== null || menuBars !== '') {
-        this.items = menuBars // 我的把菜单数据赋值给定义的this.items
-        this.todoId = menuBars[0].id // 把菜单数据的默认的第一个对象的id赋值给默认选中的id
+    getMenuList({}).then(res => {
+      const menuList = res.data // 数据都会返回在res.data里面。
+      if (menuList !== null || menuList !== '') {
+        this.items = menuList // 我的把菜单数据赋值给定义的this.items
+        this.todoId = menuList[0].id // 把菜单数据的默认的第一个对象的id赋值给默认选中的id
       }
     })
   },
@@ -48,11 +49,11 @@ export default {
       // 点击菜单时候,替换选中id
       this.todoId = id
     },
-    addTodoList () {
+    addMenuList () {
       // 点击新增按钮时候
       // 调用新增菜单的接口，在接口调用成功在请求数据
-      addTodo({}).then(data => {
-        getTodoList({}).then(res => {
+      addMenu({}).then(data => {
+        getMenuList({}).then(res => {
           const TODOS = res.data
           this.todoId = TODOS[TODOS.length - 1].id
           this.items = TODOS
